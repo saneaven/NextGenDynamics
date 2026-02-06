b167050d73780626c0c44ad89450df2a54c32494
saneaven <saneaven@gmail.com>
2026-02-06 01:08:53 -0800

    More extreme values

--- SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
+++ SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
@@ -139,7 +139,7 @@ class RewardsCfg:
     # NOTE: We keep rewards unscaled in mdp functions and apply scaling here via manager weights.
     #       This keeps the RewardManager table meaningful and avoids hiding scales inside env.cfg.* fields.
     life_time = RewTerm(func=mdp.life_time_reward, weight=0.005)
-    progress = RewTerm(func=mdp.progress_reward, weight=2.5e4)
+    progress = RewTerm(func=mdp.progress_reward, weight=5.0e4)
     velocity_alignment = RewTerm(func=mdp.velocity_alignment_reward, weight=2.5e2)
     reach_target = RewTerm(func=mdp.reach_target_reward, weight=1.0e5)
 
@@ -147,7 +147,7 @@ class RewardsCfg:
     feet_ground_time = RewTerm(func=mdp.feet_ground_time_penalty, weight=-5.0e1)
     jump_penalty = RewTerm(func=mdp.jump_penalty, weight=-2.5e3)
     body_angular_velocity = RewTerm(func=mdp.body_angular_velocity_penalty, weight=-15.0)
-    speed = RewTerm(func=mdp.speed_reward, weight=2.5e2)
+    speed = RewTerm(func=mdp.speed_reward, weight=1.0e3)
     body_vertical_acceleration = RewTerm(func=mdp.body_vertical_acceleration_penalty, weight=-3.0)
     dof_torques = RewTerm(func=mdp.dof_torques_l2, weight=-0.25)
     dof_acc = RewTerm(func=mdp.dof_acc_l2, weight=-2.5e-4)
