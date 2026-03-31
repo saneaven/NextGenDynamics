6f8255eddcaa1e6ec8e7abb8875ea92e2715197a
Minjae Kyung <saneaven@gmail.com>
2026-03-31 16:24:26 -0700

    some config adjustments

--- SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
+++ SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
@@ -145,14 +145,14 @@ class ObservationsCfg:
 class RewardsCfg:
     """Reward terms (each computes inline from sensors/robot data)."""
     life_time = RewTerm(func=mdp.life_time_reward, weight=0.005)
-    progress = RewTerm(func=mdp.progress_reward, weight=2.5e4)
-    velocity_alignment = RewTerm(func=mdp.velocity_alignment_reward, weight=5.0e2)
+    progress = RewTerm(func=mdp.progress_reward, weight=5.0e3)
+    velocity_alignment = RewTerm(func=mdp.velocity_alignment_reward, weight=2.0e2)
     reach_target = RewTerm(func=mdp.reach_target_reward, weight=5.0e2)
     death_penalty = RewTerm(func=mdp.death_penalty, weight=-3.0e2)
     feet_ground_time = RewTerm(func=mdp.feet_ground_time_penalty, weight=-5.0e1)
     jump_penalty = RewTerm(func=mdp.jump_penalty, weight=-3.0e2)
     body_angular_velocity = RewTerm(func=mdp.body_angular_velocity_penalty, weight=-15.0)
-    speed = RewTerm(func=mdp.speed_reward, weight=1.0e2)
+    speed = RewTerm(func=mdp.speed_reward, weight=5.0e1)
     body_vertical_acceleration = RewTerm(func=mdp.body_vertical_acceleration_penalty, weight=-3.0)
     dof_torques = RewTerm(func=mdp.dof_torques_l2, weight=-0.25)
     dof_acc = RewTerm(func=mdp.dof_acc_l2, weight=-2.5e-4)
@@ -198,7 +198,7 @@ class SpiderBotAIEnvCfg(ManagerBasedRLEnvCfg):
 
     # Viewer / video recording camera
     viewer: ViewerCfg = ViewerCfg(
-        eye=(3.0, 3.0, 2.5),
+        eye=(15.0, 15.0, 10),
         lookat=(0.0, 0.0, 0.8),
         cam_prim_path="/OmniverseKit_Persp",
         resolution=(1280, 720),
