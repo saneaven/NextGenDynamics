b30e40da60621d2c8383a1677eeb1868fe96ae91
saneaven <saneaven@gmail.com>
2026-04-19 14:46:35 -0700

    Update env_cfg.py

--- SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
+++ SpiderBotAIProject/source/SpiderBotAIProject/SpiderBotAIProject/tasks/manager_based/spiderbot_ai/env_cfg.py
@@ -147,14 +147,14 @@ class ObservationsCfg:
 class RewardsCfg:
     """Reward terms (each computes inline from sensors/robot data)."""
     life_time = RewTerm(func=mdp.life_time_reward, weight=0.005)
-    progress = RewTerm(func=mdp.progress_reward, weight=5.0e3)
-    velocity_alignment = RewTerm(func=mdp.velocity_alignment_reward, weight=5.0e3)
-    reach_target = RewTerm(func=mdp.reach_target_reward, weight=5.0e3)
+    progress = RewTerm(func=mdp.progress_reward, weight=5.0e2)
+    velocity_alignment = RewTerm(func=mdp.velocity_alignment_reward, weight=5.0e2)
+    reach_target = RewTerm(func=mdp.reach_target_reward, weight=5.0e2)
     death_penalty = RewTerm(func=mdp.death_penalty, weight=-1.0e1)
     feet_ground_time = RewTerm(func=mdp.feet_ground_time_penalty, weight=-5.0e1)
     jump_penalty = RewTerm(func=mdp.jump_penalty, weight=-3.0e1)
     body_angular_velocity = RewTerm(func=mdp.body_angular_velocity_penalty, weight=-1.5e0)
-    speed = RewTerm(func=mdp.speed_reward, weight=1.0e3)
+    speed = RewTerm(func=mdp.speed_reward, weight=1.0e2)
     body_vertical_acceleration = RewTerm(func=mdp.body_vertical_acceleration_penalty, weight=-1.0e0)
     dof_torques = RewTerm(func=mdp.dof_torques_l2, weight=-0.25)
     dof_acc = RewTerm(func=mdp.dof_acc_l2, weight=-2.5e-4)
@@ -165,7 +165,7 @@ class RewardsCfg:
     flat_orientation = RewTerm(func=mdp.flat_orientation_l2, weight=-1.0e1)
     wall_proximity = RewTerm(func=mdp.wall_proximity_penalty, weight=-1.0e1)
 
-    patrol_exploration = RewTerm(func=mdp.patrol_exploration_reward, weight=2.0e3)
+    patrol_exploration = RewTerm(func=mdp.patrol_exploration_reward, weight=1.0e2)
     patrol_boundary = RewTerm(func=mdp.patrol_boundary_penalty, weight=-2.5e1)
 
     chase_proximity = RewTerm(func=mdp.chase_proximity_reward, weight=1.0e2)
